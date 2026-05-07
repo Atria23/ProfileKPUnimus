0db549a2e1b704d1b21741f9027fc542b36da26b
atria23 <danu03anggoro@gmail.com>
2026-05-07 13:19:49 +0700

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-// 'tailwindcss' sudah termasuk dalam laravel-vite-plugin, jadi Anda mungkin tidak perlu ini
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    // Tambahkan bagian 'server' ini
-    // yang server di bawah sesuaikan dengan ip host ipv4 dari wifi yang dipakai serta menjalankan "php artisan serve --host=0.0.0.0 --port=8000" agar bisa dibuka dari hp maupun laptop (kalo yang port itu bebas, tapi kalo host itu dibuat gitu)
-    // cara buka nya di semua komputer yaitu dengan mengakses http://localhost:8000/
-    server: {
-        host: '0.0.0.0',
-        port: 5174,
-        hmr: {
-            host: '10.230.151.48',
-        },
-        cors: true
-    },
+export default defineConfig(({ command }) => ({
+    server:
+        command === 'serve'
+            ? {
+                  host: '0.0.0.0',
+                  port: 5174,
+                  hmr: {
+                      host: '10.230.151.48',
+                  },
+                  cors: true,
+              }
+            : undefined,
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
@@ -22,4 +22,4 @@ export default defineConfig({
         }),
         react(),
     ],
-});
+}));
